0f92c1a9a78089d362331c99e67ec97291a091b0
caopeiqi <18335929360@163.com>
2021-12-01 00:09:27 +0800

    头部

---
+++ src/views/vis-home-page/home-header/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="vis-homeHeader">
+        <div class="homeHeader_time">
+            {{ timeData }}
+        </div>
+        <span class="homeHeader_title">
+            邯郸区司法局指挥中心
+        </span>
+    </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+console.log(dayjs, 'dayyyyyyyyyyy')
+export default {
+    name: 'homeHeader',
+    data() {
+        return {
+            timeData: '' // 系统当前时间显示
+        }
+    },
+    mounted() {
+        this.getTimeData()
+    },
+    methods: {
+        getTimeData() {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.vis-homeHeader {
+    width: 100%;
+    height: 100%;
+    background: url(../../../assets/homePage/header.png) no-repeat center center;
+    background-size: 100% 100%;
+}
+.homeHeader_title {
+    line-height: 100%;
+    color: #fff;
+    display: inline-block;
+    margin: 0 auto;
+
+}
+</style>
--- src/views/vis-home-page/index.vue
+++ src/views/vis-home-page/index.vue
@@ -1,8 +1,23 @@
 <template>
     <div class='vis-homePage'>
-        首页
+        <div class='vis-homePage_header'>
+            <home-header></home-header>
+        </div>
+        <div class='vis-homePage_container'>
+            
+        </div>
     </div>
 </template>
+
+<script>
+import homeHeader from './home-header/index'
+export default {
+    components: {
+        homeHeader
+    }
+}
+</script>
+
 
 <style scoped>
 .vis-homePage {
@@ -10,5 +25,10 @@
     height: 100%;
     background: url('../../assets/login/bg.png') no-repeat center center;
     background-size: 100% 100%;
+}
+.vis-homePage_header {
+    width: 100%;
+    height: 10%;
+    /* height: 296px; */
 }
 </style>
