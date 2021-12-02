2b178a61efe6ce4db2128fc2a0b05cb25c5dd70c
caopeiqi <18335929360@163.com>
2021-12-02 21:52:01 +0800

    布局

---
+++ src/views/vis-home-page/home-container/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="vis-homeContainer">
+        <div class="homeContainer_charts">
+            左侧
+        </div>
+        <div class="homeContainer_map">
+            地图
+        </div>
+        <div class="homeContainer_charts">
+            右侧
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    }
+}
+</script>
+
+<style lang="less">
+.vis-homeContainer {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    .homeContainer_charts {
+        width: 2000px;
+        height: 100%;
+        background: red;
+    }
+    .homeContainer_map {
+        width: 3520px;
+        height: 100%;
+        background: coral;
+    }
+}
+</style>
--- src/views/vis-home-page/index.vue
+++ src/views/vis-home-page/index.vue
@@ -4,33 +4,39 @@
             <home-header></home-header>
         </div>
         <div class='vis-homePage_container'>
-            
+            <home-container></home-container>
         </div>
     </div>
 </template>
 
 <script>
 import homeHeader from './home-header/index'
+import homeContainer from './home-container'
 export default {
     components: {
-        homeHeader
+        homeHeader,
+        homeContainer
     }
 }
 </script>
 
-
-<style scoped>
+<style lang="less">
 .vis-homePage {
     width: 100%;
     height: 100%;
     background: url('../../assets/login/bg.png') no-repeat center center;
     background-size: 100% 100%;
-}
-.vis-homePage_header {
-    width: 100%;
-    /* height: 10%; */
-    height: 296px;
-    background: url(../../assets/homePage/header.png) no-repeat center center;
-    background-size: 100% 100%;
+    &_header {
+        width: 100%;
+        /* height: 10%; */
+        height: 296px;
+        background: url(../../assets/homePage/header.png) no-repeat center center;
+        background-size: 100% 100%;
+    }
+    &_container {
+        width: calc(100% - 80px);
+        height: 1810px;
+        margin: 40px auto;
+    }
 }
 </style>
